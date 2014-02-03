150b8867ec9169a34f2868c49c368ae222df588c
mhurak <michalhurak@MBpro.local>
2014-02-03 18:38:00 +0100

    Aktualny admin prihlaseny implementacia

--- app/controllers/admin_sessions_controller.rb
+++ app/controllers/admin_sessions_controller.rb
@@ -11,7 +11,7 @@ class AdminSessionsController < ApplicationController
     admin_user = AdminUser.where(email: params[:email]).first
 
     if admin_user && admin_user.authenticate(params[:password])
-      session[:logged] = true
+      session[:admin_user_id] = admin_user.id
       redirect_to admin_posts_url
     else
       flash[:alert] = "Nesprávny email alebo heslo!"
@@ -20,7 +20,7 @@ class AdminSessionsController < ApplicationController
   end
 
   def destroy
-    session[:logged] = nil
+    session[:admin_user_id] = nil
     flash[:alert] = "Bol si odhláseny!"
     redirect_to new_admin_session_url
   end
